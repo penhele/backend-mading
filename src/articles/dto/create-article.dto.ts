1651ce0a5a50fa775b7b1565b16b20d13d954955
Stephen Helenus <stephenhelk@gmail.com>
2026-06-28 01:00:09 +0700

    feat: mengubah categoryName menjadi categoryId di relasinya

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,3 +1,25 @@
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 
+export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
 
-export class CreateArticleDto {}
+  @IsString()
+  @IsNotEmpty()
+  content!: string;
+
+  @IsOptional()
+  image_url!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  status!: string;
+
+  @IsUUID()
+  userId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  catego
+}
